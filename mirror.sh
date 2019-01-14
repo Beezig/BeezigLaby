613f987642c846e277a48133461e70f80592206e
Travis CI <travis@travis-ci.org>
2019-01-14 12:39:17 +0100

    Mirror all submodules to src root

--- mirror.sh
+++ mirror.sh
@@ -4,15 +4,31 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git clone --recursive https://github.com/Beezig/BeezigLaby.git # Clone the project into the BeezigLaby directory
+echo "Cloning the repository..."
+git clone --recursive https://github.com/Beezig/BeezigLaby.git -q &> /dev/null  # Clone the project into the BeezigLaby directory, silently
 cd BeezigLaby # cd into the directory
 
 # Remove the references to the submodules
 rm -rf .git
 rm .gitmodules
 
+# Copy the contents of the submodules into the root folder
+rm Beezig/src/pw
+
+mv -v Beezig/src/* src/main/java
+ls -a
+mv submodules/src/* src/main/java
+mv BeezigForge/src/main/java/* src/main/java
+
+rm -rf Beezig
+rm -rf BeezigForge
+
+mv src/main/java/libraries/* src/main/resources
+mv src/main/java/lang/* src/main/resources
+mv src/main/java/core/messages/* src/main/resources
+
 git init # Create a blank repository
 git remote add origin https://${GIT_TOKEN}@github.com/RoccoDev/BeezigLaby-Mirror.git # Add the mirror as remote
-git add --all # Add all the files
-git commit -m "BeezigLaby mirror commit" # Commit changes
-git push origin master --force # Force push the repository
+git add --all -q &> /dev/null# Add all the files 
+git commit -m "BeezigLaby mirror commit" -q &> /dev/null # Commit changes
+git push origin master --force -q &> /dev/null # Force push the repository
