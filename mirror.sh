5c35f2f38871d352d6d9642e6b74b62dfcb127d1
Travis CI <travis@travis-ci.org>
2019-01-14 12:48:32 +0100

    This will hopefully fix the cd issues

--- mirror.sh
+++ mirror.sh
@@ -4,8 +4,7 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-echo "Cloning the repository..."
-git clone --recursive https://github.com/Beezig/BeezigLaby.git &> /dev/null  # Clone the project into the BeezigLaby directory, silently
+git clone --recursive https://github.com/Beezig/BeezigLaby.git # Clone the project into the BeezigLaby directory
 cd BeezigLaby # cd into the directory
 
 # Remove the references to the submodules
