409c7cf0dd1477bf7290f313746078c27d479f99
RoccoDev <roccodev.business@gmail.com>
2020-05-21 14:17:42 +0200

    Trivial mirroring

--- mirror.sh
+++ mirror.sh
@@ -4,37 +4,13 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-resourceDirs=(lang core META-INF assets)
-./gradlew build -DFORGESRG=true
 cd build/libs
-mkdir extract
-mkdir -p newSrc/src/main/java
-mkdir -p newSrc/src/main/resources
-cp BeezigLaby-*.jar extract
-
-# Extract jar for resources
-cd extract
+mkdir -p unzipped/src/main/resources
+cp BeezigLaby-*.jar unzipped/src/main/resources
+cd unzipped/src/main/resources
 unzip BeezigLaby-*.jar
 rm BeezigLaby-*.jar
-for dir in "${resourceDirs[@]}"
-do
-    mv $dir ../newSrc/src/main/resources
-done
-rm -rf $(find * -prune -type d)
-mv * ../newSrc/src/main/resources
-cd ..
-
-wget https://rocco.dev/beezighosting/cfr.jar
-wget https://rocco.dev/beezighosting/bon.jar
-rm BeezigLaby-*-sources.jar
-java -jar bon.jar --inputJar BeezigLaby-*.jar --mappingsVer 22 --outputJar BeezigLaby-deobf.jar
-java -jar cfr.jar BeezigLaby-deobf.jar --outputdir newSrc/src/main/java
-cd newSrc/src/main/java
-find . -name "package-info.java" -delete
-rm -r ../resources/META-INF
 cd ../../../
-cp ../../../EMBED .
-cp ../../../PROVIDE .
 cp -r ../../../libs .
 mv ../../../MIRROR.md ./README.md
 git init # Create a blank repository
@@ -43,4 +19,3 @@ git add --all # Add all the files
 git commit -m "BeezigLaby mirror commit" # Commit changes
 git push origin master --force # Force push the repository
 cd ../../../
-./gradlew clean
