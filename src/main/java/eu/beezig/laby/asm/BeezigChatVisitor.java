da33b1be945fcd475274e44b9d29a5704d8beb9f
Archer <archer@beezig.eu>
2020-08-08 20:38:31 +0200

    copyright: Update copyright in asm
    Updated copyright in BeezigLaby ASM classes.
    
    Signed-off-by: Archer <archer@beezig.eu>

--- src/main/java/eu/beezig/laby/asm/BeezigChatVisitor.java
+++ src/main/java/eu/beezig/laby/asm/BeezigChatVisitor.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (C) 2019 Beezig (RoccoDev, ItsNiklass)
+ * Copyright (C) 2017-2020 Beezig Team
  *
- * This file is part of BeezigLaby.
+ * This file is part of Beezig.
  *
- * BeezigLaby is free software: you can redistribute it and/or modify
+ * Beezig is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * BeezigLaby is distributed in the hope that it will be useful,
+ * Beezig is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with BeezigLaby.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Beezig.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package eu.beezig.laby.asm;
