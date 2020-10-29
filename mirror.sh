a4936888b5b5be11ed0892e6b2eb880b1dd13a7e
RoccoDev <roccodev.business@gmail.com>
2020-10-29 17:34:32 +0000

    ci: use cache for mirror
    Also cache the entire build directory

--- mirror.sh
+++ mirror.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -e
 
 # Setup git
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "Travis CI"
+git config --global user.email "gitlab-bot@beezig.eu"
+git config --global user.name "Beezig Bot"
 
 cd build/libs
 mkdir -p unzipped/src/main/resources
@@ -19,7 +20,7 @@ touch PROVIDE
 touch EMBED
 mv ../../../MIRROR.md ./README.md
 git init # Create a blank repository
-git remote add origin https://${GIT_TOKEN}@github.com/RoccoDev/BeezigLaby-Mirror.git # Add the mirror as remote
+git remote add origin https://beezig-bot:${MIRROR_KEY}@gitlab.com/Beezig/BeezigLaby-Mirror.git # Add the mirror as remote
 git add --all # Add all the files
 git commit -m "BeezigLaby mirror commit" # Commit changes
 git push origin master --force # Force push the repository
